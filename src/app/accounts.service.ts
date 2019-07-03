3cca0c6ae601b8814cf7356e99583d51f3ef0fbe
storres8 <stevetorres7562@gmail.com>
2019-07-03 10:48:52 -0400

    updated main app component to use accounts service component.

---
+++ src/app/accounts.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: "root"
+})
+export class AccountsService {
+  constructor() {}
+  accounts = [
+    {
+      name: "Master Account",
+      status: "active"
+    },
+    {
+      name: "Testaccount",
+      status: "inactive"
+    },
+    {
+      name: "Hidden Account",
+      status: "unknown"
+    }
+  ];
+
+  addAccount(name: string, status: string) {
+    this.accounts.push({ name: name, status: status });
+  }
+  updateStatus(id: number, status: string) {
+    this.accounts[id].status = status;
+  }
+}
