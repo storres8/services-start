9406f7ab143775f08f01e8f9ff02fd6ec070b897
storres8 <stevetorres7562@gmail.com>
2019-07-03 11:17:43 -0400

    added logging service to all componented through heirachial injector.

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component } from "@angular/core";
 import { LoggingService } from "../logging.service";
+import { AccountsService } from "../accounts.service";
 
 @Component({
   selector: "app-new-account",
@@ -8,18 +9,20 @@ import { LoggingService } from "../logging.service";
   providers: [LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
-
   // This line of code in the constructor is what injects an instance of our logging service component
   // into this component. MUST have the type equal to the name of the service component to be injected.
   // Must also include the providers array above with the service component inside.
-  constructor(private loggingService: LoggingService) {}
+  constructor(
+    private loggingService: LoggingService,
+    private accountsService: AccountsService
+  ) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    // this.accountAdded.emit({
+    //   name: accountName,
+    //   status: accountStatus
+    // });
+    this.accountsService.addAccount(accountName, accountStatus);
     this.loggingService.logStatusChange(accountStatus);
   }
 }
